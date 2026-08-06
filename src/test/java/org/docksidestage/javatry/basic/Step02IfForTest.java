930c75bbba0f10bd1d002b36cc9eb8bc547dc905
kazuki-okamura-biz <kazuki.okamura@bizreach.co.jp>
2026-08-06 18:18:05 +0900

    step02

--- src/test/java/org/docksidestage/javatry/basic/Step02IfForTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step02IfForTest.java
@@ -52,7 +52,7 @@ public class Step02IfForTest extends PlainTestCase {
         } else {
             sea = 7;
         }
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 7
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
@@ -67,7 +67,7 @@ public class Step02IfForTest extends PlainTestCase {
         } else {
             sea = 9;
         }
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 7
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
@@ -82,8 +82,9 @@ public class Step02IfForTest extends PlainTestCase {
             sea = ++sea * 2;
         } else if (sea >= 903 || land) {
             if (sea % 2 == 0) {
-                sea = sea++ * 2;
+                sea = sea++ * 2; //1809? => 1808
             }
+            log(sea);
             if (!land) {
                 land = true;
             } else if (sea <= 903) {
@@ -107,7 +108,9 @@ public class Step02IfForTest extends PlainTestCase {
         if (land) {
             sea = 10;
         }
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 10
+        // sea = sea++ * 2;　について、sea++ * 2;の計算が終わったらseaは905になるが、計算結果が代入されて1808になる？
+        // sea++の場合、計算には元のseaの値が使われるのは知ってたけど、どのタイミングでインクリメントされるかが曖昧だった
     }
 
     // ===================================================================================
@@ -123,7 +126,7 @@ public class Step02IfForTest extends PlainTestCase {
                 sea = stage;
             }
         }
-        log(sea); // your answer? => 
+        log(sea); // your answer? => dockside
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
@@ -133,7 +136,8 @@ public class Step02IfForTest extends PlainTestCase {
         for (String stage : stageList) {
             sea = stage;
         }
-        log(sea); // your answer? => 
+        log(sea); // your answer? => magiclamp
+        // for (String stage : stageList) で各ループでstageにstageListの値が順番に入る
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
@@ -149,7 +153,7 @@ public class Step02IfForTest extends PlainTestCase {
                 break;
             }
         }
-        log(sea); // your answer? => 
+        log(sea); // your answer? => hangar
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
@@ -165,7 +169,8 @@ public class Step02IfForTest extends PlainTestCase {
             }
         });
         String sea = sb.toString();
-        log(sea); // your answer? => 
+        log(sea); // your answer? => dockside
+        // stageList.forEach(stage -> と for (String stage : stageList) は同じ挙動？
     }
 
     // ===================================================================================
@@ -177,6 +182,16 @@ public class Step02IfForTest extends PlainTestCase {
      */
     public void test_iffor_making() {
         // write if-for here
+        List<String> stageList = prepareStageList();
+        List<String> ansList = new ArrayList<>();
+        for (String stage : stageList){
+            if(stage.contains("a")) {
+                ansList.add(stage);
+            }
+        }
+        for (String ans : ansList){
+            log(ans);
+        }
     }
 
     // ===================================================================================
@@ -186,33 +201,69 @@ public class Step02IfForTest extends PlainTestCase {
      * Change foreach statement to List's forEach() (keep result after fix) <br>
      * (foreach文をforEach()メソッドへの置き換えてみましょう (修正前と修正後で実行結果が同じになるように))
      */
+//    public void test_iffor_refactor_foreach_to_forEach() {
+//        List<String> stageList = prepareStageList();
+//        String sea = null;
+//        stageList.forEach(stage -> {
+//            if (stage.startsWith("br")) {
+//                return;
+//            }
+//            sea = stage;
+//            if (stage.contains("ga")) {
+//                break;
+//            }
+//        });
+//        log(sea); // should be same as before-fix
+//    }
+
+    // 何も見ずに書こうとしたらここまでしか出ませんでした...
+    // forEach()メソッドの中でseaの代入ができない！breakが使えない！
+
     public void test_iffor_refactor_foreach_to_forEach() {
+        StringBuilder flag = new StringBuilder();
         List<String> stageList = prepareStageList();
-        String sea = null;
-        for (String stage : stageList) {
-            if (stage.startsWith("br")) {
-                continue;
+        StringBuilder sea = new StringBuilder();
+        stageList.forEach(stage -> {
+            if (flag.length()>0 || stage.startsWith("br")) {
+                return;
             }
-            sea = stage;
+            sea.setLength(0);
+            sea.append(stage);
             if (stage.contains("ga")) {
-                break;
+                flag.append("ga");
             }
-        }
+        });
         log(sea); // should be same as before-fix
     }
+    // このコードは調べたりAI使いながら自分の考えをコードにしました
+    // continue->return
+    // 変数の代入はできないので、StringBuilderを用いて、空にしてからappend
+    // breakに相当するものは無い？
 
     /**
      * Make your original exercise as question style about if-for statement. <br>
      * (if文for文についてあなたのオリジナルの質問形式のエクササイズを作ってみましょう)
      * <pre>
      * _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
-     * your question here (ここにあなたの質問を):
+     * your question here (ここにあなたの質問を): 変数seaの値は？
      * 
      * _/_/_/_/_/_/_/_/_/_/
      * </pre>
      */
     public void test_iffor_yourExercise() {
         // write your code here
+        int sea = 0;
+        for(int i=0; i<10; i++){
+            sea += 7;
+            if(sea % 2 == 0){
+                continue;
+            }
+            sea -= 4;
+            if(sea > 10){
+                break;
+            }
+        }
+        log(sea);
     }
 
     // ===================================================================================
